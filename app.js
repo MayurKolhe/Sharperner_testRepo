cdae90065fed99e566f0a9ff6ec6744990ffb3b7
“Mayur <mayurkolhe1995@csu.fullerton.edu>
2023-10-30 19:49:11 -0700

    commit changes for the DOM manipulation

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log("I love git");
+// console.dir(document);
+// const domainName = document.domain;
+// console.log(domainName);
+
+
+// console.log(document.URL);
+// console.log(document.title);
+
+// document.title = "Mayur";
+
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// document.all[15].textContent = "My Personal Items";
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+
+// Hand on DOM Elements using getElementbyId
+
+// console.log(document.getElementById("items"));
+// const mainTitle = document.getElementById("header-title");
+// console.log(mainTitle);
+
+// mainTitle.textContent = "Hello World";
+// mainTitle.innerText = "Good Bye";
+
+// console.log(mainTitle.innerText);
+
+// mainTitle.innerHTML = "<h3> Welcome Mayur </h3>";
+
+// mainTitle.style.borderBottom = "solid 3px #000"
+
+const title = document.getElementsByClassName("title");
+
+const heading = title[0];
+
+heading.style.fontWeight = "bold";
+heading.style.color = "green";
+
+
+
+
