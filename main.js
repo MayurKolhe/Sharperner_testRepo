935c5ce3fb86a75794ff88ef39c2882a8fe57ea8
“Mayur <mayurkolhe1995@csu.fullerton.edu>
2023-11-05 00:22:17 -0700

    committing  for the Delete data from Local Storage

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const myform = document.getElementById("my-form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const msg = document.querySelector(".msg");
-const userList = document.querySelector("#users");
+const userList = document.getElementById("users");
 
 const onSubmit = (e) => {
   e.preventDefault();
-    let currentvalue = {
-        name: "",
-        email: ""
+  let currentvalue = {
+    name: "",
+    email: "",
   };
   if (nameInput === "" || emailInput === "") {
     msg.classList.add("error");
@@ -17,16 +17,21 @@ const onSubmit = (e) => {
     setTimeout(() => msg.remove, 3000);
   } else {
     const li = document.createElement("li");
+    const delebutton = document.createElement("button");
+    delebutton.className = "btn btn-danger btn-sm float-right delete";
+    delebutton.appendChild(document.createTextNode("X"));
     li.appendChild(
       document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
     );
+    li.appendChild(delebutton);
+    li.className = "item";
     userList.appendChild(li);
-      currentvalue.name = nameInput.value;
-      currentvalue.email = emailInput.value;
+    currentvalue.name = nameInput.value;
+    currentvalue.email = emailInput.value;
     nameInput.value = "";
     emailInput.value = "";
-  }   
-  
+  }
+
   if (localStorage.getItem(currentvalue.email)) {
     let current = localStorage.getItem(currentvalue.email);
     current = current + ", " + JSON.stringify(currentvalue);
@@ -37,3 +42,28 @@ const onSubmit = (e) => {
 };
 
 myform.addEventListener("submit", onSubmit);
+
+const deleteitems = (e) => {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you sure want to exit")) {
+        const li = e.target.parentElement;
+        const data = li.textContent;
+        const emailMatch = data.match(/: (.+)$/);
+        let email = emailMatch[1]
+        userList.removeChild(li);
+        let key = emailMatch[1].substring(0, email.length-1);
+        console.log(key);
+       
+        localStorage.removeItem(key);
+    }
+  }
+};
+
+userList.addEventListener("click", deleteitems);
+
+// for (let i = 0; i < userList.length; i++) {
+//   const delebutton = document.createElement("button");
+//   delebutton.className = "btn btn-danger btn-sm float-right delete";
+//   delebutton.appendChild(document.createTextNode("X"));
+//   userList[i].appendChild(delebutton);
+// }
