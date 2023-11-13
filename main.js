ed51902cb2c4ab03ffdf49f5f4a91f3f14797943
“Mayur <mayurkolhe1995@csu.fullerton.edu>
2023-11-12 16:36:07 -0800

    Add edit button to list items and implement edit functionality

--- main.js
+++ main.js
@@ -18,12 +18,16 @@ const onSubmit = (e) => {
   } else {
     const li = document.createElement("li");
     const delebutton = document.createElement("button");
+    const editButton = document.createElement("button");
     delebutton.className = "btn btn-danger btn-sm float-right delete";
+    editButton.className = "btn btn-primary btn-sm float-right custom-action";
     delebutton.appendChild(document.createTextNode("X"));
+    editButton.appendChild(document.createTextNode("Edit"));
     li.appendChild(
       document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
     );
     li.appendChild(delebutton);
+    li.appendChild(editButton);
     li.className = "item";
     userList.appendChild(li);
     currentvalue.name = nameInput.value;
@@ -46,21 +50,41 @@ myform.addEventListener("submit", onSubmit);
 const deleteitems = (e) => {
   if (e.target.classList.contains("delete")) {
     if (confirm("Are you sure want to exit")) {
-        const li = e.target.parentElement;
-        const data = li.textContent;
-        const emailMatch = data.match(/: (.+)$/);
-        let email = emailMatch[1]
-        userList.removeChild(li);
-        let key = emailMatch[1].substring(0, email.length-1);
-        console.log(key);
-       
-        localStorage.removeItem(key);
+      const li = e.target.parentElement;
+      const data = li.textContent;
+      const emailMatch = data.match(/: (.+)$/);
+      let email = emailMatch[1];
+      userList.removeChild(li);
+      let key = emailMatch[1].substring(0, email.length - 1);
+      localStorage.removeItem(key);
     }
   }
 };
 
-userList.addEventListener("click", deleteitems);
+const editButton = (e) => {
+  e.preventDefault();
+  let email = "";
+  let name = "";
+  if (e.target.classList.contains("custom-action")) {
+    if (confirm("Are you sure want to Edit")) {
+      const li = e.target.parentElement;
+      const data = li.textContent;
+      const emailMatch = data.match(/: (.+)$/);
+      email = emailMatch[1].replace("Edit", "").replace("X", "");
+      console.log(email);
+      let key = JSON.parse(localStorage.getItem(email));
+      name = key.name;
+      userList.removeChild(li);
+      localStorage.removeItem(email);
+    }
+  }
+  nameInput.value = name;
+  emailInput.value = email;
+};
 
+
+userList.addEventListener("click", deleteitems);
+userList.addEventListener("click", editButton);
 // for (let i = 0; i < userList.length; i++) {
 //   const delebutton = document.createElement("button");
 //   delebutton.className = "btn btn-danger btn-sm float-right delete";
